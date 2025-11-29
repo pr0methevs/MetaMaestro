fe0277e78283386495825f20db9b782ccfa99c8a
GitHub Action <action@github.com>
2025-11-29 14:41:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/pr0methevs/metamaestro/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/pr0methevs/metamaestro/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.pr0methevs.metamaestro.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pr0methevs.metamaestro.MyBundle
+import com.github.pr0methevs.metamaestro.services.MyProjectService
 import javax.swing.JButton
 
 
